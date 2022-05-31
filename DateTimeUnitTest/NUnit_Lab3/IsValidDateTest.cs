61d6fa4b13b52c548cf2d32f03dcd151ae1b77a9
truongquangtan <truongquangtan2001@gmail.com>
2022-05-31 09:29:01 +0700

    Add

--- DateTimeUnitTest/NUnit_Lab3/IsValidDateTest.cs
+++ DateTimeUnitTest/NUnit_Lab3/IsValidDateTest.cs
@@ -3,10 +3,71 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit;
+using NUnit.Framework;
+using DateTimeCheckingWeb.Supporters;
 
 namespace DateTimeUnitTest.NUnit_Lab3
 {
-    internal class IsValidDateTest
+    [TestFixture]
+    public class IsValidDateTest
     {
+        [Test]
+        public void TestDayInMonth_UTCID01()
+        {
+            int actual = DateValidator.DaysInMonth(2020, 1);
+            int expected = 31;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestDayInMonth_UTCID02()
+        {
+            int actual = DateValidator.DaysInMonth(2020, 1);
+            int expected = 31;
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void TestDayInMonth_UTCID03()
+        {
+            int actual = DateValidator.DaysInMonth(2020, 1);
+            int expected = 31;
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void TestDayInMonth_UTCID04()
+        {
+            int actual = DateValidator.DaysInMonth(2020, 1);
+            int expected = 31;
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void TestDayInMonth_UTCID05()
+        {
+            int actual = DateValidator.DaysInMonth(2020, 1);
+            int expected = 31;
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void TestDayInMonth_UTCID06()
+        {
+            int actual = DateValidator.DaysInMonth(2020, 1);
+            int expected = 31;
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void TestDayInMonth_UTCID07()
+        {
+            int actual = DateValidator.DaysInMonth(2020, 1);
+            int expected = 31;
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void TestDayInMonth_UTCID08()
+        {
+            int actual = DateValidator.DaysInMonth(2020, 1);
+            int expected = 31;
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
